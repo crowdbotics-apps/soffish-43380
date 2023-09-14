acb9c2770f0040976e4290f0b9babc7f5d0e5093
Crowdbotics <team@crowdbotics.com>
2023-09-14 08:32:15 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "soffish-43380",
+  url: "https://soffish-43380.botics.co",
+  api: "https://soffish-43380.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
